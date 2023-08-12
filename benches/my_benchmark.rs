91545d0a8695a32926a31cc50a9b05b29fcd7060
Yann Prono <yann.prono@maif.fr>
2023-08-12 23:34:34 +0200

    linux 6.4.10

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -4,18 +4,54 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use nom_kconfig::{kconfig::parse_kconfig, KconfigFile, KconfigInput};
 
 fn criterion_benchmark(c: &mut Criterion) {
-    let input_file = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
-        .join("tests")
-        .join("Kconfig");
-    let kconfig_file: KconfigFile = KconfigFile::new(
-        input_file.parent().unwrap().to_path_buf(),
-        input_file.clone(),
-    );
+    c.bench_function("parse_kconfig_1_config_entry", |b| {
+        b.iter(|| {
+            let input_file = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+                .join("benches")
+                .join("linux-6.4.10")
+                .join("drivers/accel/ivpu/Kconfig");
+            let kconfig_file: KconfigFile = KconfigFile::new(
+                input_file.parent().unwrap().to_path_buf(),
+                input_file.clone(),
+            );
+            let content: String = kconfig_file.read_to_string().unwrap();
+            let input: nom_locate::LocatedSpan<&str, KconfigFile> = KconfigInput::new_extra(&content, kconfig_file);
+            let _ = parse_kconfig(black_box(input.clone()));
+        })
+    });
 
-    let content = kconfig_file.read_to_string().unwrap();
-    let input = KconfigInput::new_extra(&content, kconfig_file);
+
+
+    c.bench_function("parse_kconfig_lot_of_sources", |b| {
+        b.iter(|| {
+            let input_file = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+                .join("benches")
+                .join("linux-6.4.10")
+                .join("arch/arm/Kconfig");
+            let kconfig_file: KconfigFile = KconfigFile::new(
+                input_file.parent().unwrap().to_path_buf(),
+                input_file.clone(),
+            );
+            let content: String = kconfig_file.read_to_string().unwrap();
+            let input: nom_locate::LocatedSpan<&str, KconfigFile> = KconfigInput::new_extra(&content, kconfig_file);
+            let _ = parse_kconfig(black_box(input.clone()));
+        })
+    });
+        
     c.bench_function("parse_kconfig", |b| {
-        b.iter(|| parse_kconfig(black_box(input.clone())))
+        b.iter(|| {
+            let input_file = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+                .join("benches")
+                .join("linux-6.4.10")
+                .join("Kconfig");
+            let kconfig_file: KconfigFile = KconfigFile::new(
+                input_file.parent().unwrap().to_path_buf(),
+                input_file.clone(),
+            );
+            let content: String = kconfig_file.read_to_string().unwrap();
+            let input: nom_locate::LocatedSpan<&str, KconfigFile> = KconfigInput::new_extra(&content, kconfig_file);
+            let _ = parse_kconfig(black_box(input.clone()));
+        })
     });
 }
 
