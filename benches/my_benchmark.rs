da3ae3fce2ed56044e991699a7a9c2b404043ecd
Yann Prono <yann.prono@maif.fr>
2023-08-17 23:18:03 +0200

    add codecov

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -1,6 +1,6 @@
 use std::path::PathBuf;
 
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{black_box, criterion_group, criterion_main};
 use nom_kconfig::{kconfig::parse_kconfig, KconfigFile, KconfigInput};
 
 fn criterion_benchmark(c: &mut Criterion) {
