c59181de2f3eb7f3cbb2829c81a2d84cb0aaa9fa
Igor Bezzubchenko <ibezzubchenko@cloudlinux.com>
2025-04-16 17:42:08 +0200

    Ability to specify variables that included sources file depend on

--- src/entry/source.rs
+++ src/entry/source.rs
@@ -34,38 +34,55 @@ pub fn parse_source(input: KconfigInput) -> IResult<KconfigInput, Source> {
         delimited(tag("\""), parse_filepath, tag("\"")),
         parse_filepath,
     )))(input)?;
-    let source_kconfig_file = KconfigFile::new(input.clone().extra.root_dir, PathBuf::from(file));
-    if is_dynamic_source(file) {
-        return Ok((
+    if let Some(file) = apply_vars(file, &input.extra.vars) {
+        let source_kconfig_file = KconfigFile::new_with_vars(
+            input.clone().extra.root_dir, PathBuf::from(file), &input.extra.vars);
+        let source_content = source_kconfig_file
+            .read_to_string()
+            .map_err(|_| nom::Err::Error(Error::from_error_kind(input.clone(), ErrorKind::Fail)))?;
+
+        let binding = source_content.clone();
+        #[allow(clippy::let_and_return)]
+        let x = match cut(parse_kconfig)(KconfigInput::new_extra(
+            &binding,
+            source_kconfig_file.clone(),
+        )) {
+            Ok((_, kconfig)) => Ok((input, kconfig)),
+            Err(_e) => Err(nom::Err::Error(nom::error::Error::new(
+                KconfigInput::new_extra("", source_kconfig_file),
+                ErrorKind::Fail,
+            ))),
+        };
+        x
+    } else {
+        Ok((
             input,
             Source {
                 file: file.to_string(),
                 ..Default::default()
             },
-        ));
+        ))
     }
-    let source_content = source_kconfig_file
-        .read_to_string()
-        .map_err(|_| nom::Err::Error(Error::from_error_kind(input.clone(), ErrorKind::Fail)))?;
-
-    let binding = source_content.clone();
-    #[allow(clippy::let_and_return)]
-    let x = match cut(parse_kconfig)(KconfigInput::new_extra(
-        &binding,
-        source_kconfig_file.clone(),
-    )) {
-        Ok((_, kconfig)) => Ok((input, kconfig)),
-        Err(_e) => Err(nom::Err::Error(nom::error::Error::new(
-            KconfigInput::new_extra("", source_kconfig_file),
-            ErrorKind::Fail,
-        ))),
-    };
-    x
 }
 
-fn is_dynamic_source(file: &str) -> bool {
-    let re = Regex::new("\\$(.+)").unwrap();
-    re.is_match(file)
+pub fn apply_vars(file: &str, extra_vars: &std::collections::HashMap<String, String>) -> Option<String> {
+    let re = Regex::new(r"\$\((\S+)\)").unwrap();
+    let mut file_copy = String::from(file);
+    for (var_name, var_value) in re
+        .captures_iter(file)
+        .map(|cap| {
+            let ex: (&str, [&str; 1]) = cap.extract();
+            let var = ex.1[0];
+            (var, extra_vars.get(var))
+        })
+    {
+        if let Some(var_value) = var_value {
+            file_copy = file_copy.replace(&format!("$({var_name})"), &var_value);
+        } else {
+            return None;
+        }
+    }
+    Some(file_copy)
 }
 
 /// Entry that reads the specified configuration file. This file is always parsed.
