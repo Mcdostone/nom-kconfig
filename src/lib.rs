cc331e26fccdecc539d6a08655ed0cc33073efbf
Yann Prono <yann.prono@telecomnancy.net>
2025-04-21 13:45:58 +0200

    docs: example of how to specify variables when parsing kconfig files

--- src/lib.rs
+++ src/lib.rs
@@ -7,12 +7,16 @@
 //! ```no_run
 //! use std::path::PathBuf;
 //! use nom_kconfig::{parse_kconfig, KconfigInput, KconfigFile};
+//! use std::collections::HashMap;
 //!
 //! // curl https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.4.9.tar.xz | tar -xJ -C /tmp/
 //! fn main() -> Result<(), Box<dyn std::error::Error>> {
-//!     let kconfig_file = KconfigFile::new(
+//!     let mut variables = HashMap::new();
+//!     variables.insert("SRCARCH", "x86");
+//!     let kconfig_file = KconfigFile::new_with_vars(
 //!         PathBuf::from("/tmp/linux-6.4.9"),
-//!         PathBuf::from("/tmp/linux-6.4.9/Kconfig")
+//!         PathBuf::from("/tmp/linux-6.4.9/Kconfig"),
+//!         &variables
 //!     );
 //!     let input = kconfig_file.read_to_string().unwrap();
 //!     let kconfig = parse_kconfig(KconfigInput::new_extra(&input, kconfig_file));
